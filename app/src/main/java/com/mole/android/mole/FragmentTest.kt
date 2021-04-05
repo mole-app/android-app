4a572cce44865a26695858ec0216a9a9bfd6bb4a
PaladiRka <milakins@bk.ru>
2021-04-05 09:47:58 +0300

    Add custom view: combining fab, materialButton, appBar and bottomNavigation.

---
+++ app/src/main/java/com/mole/android/mole/FragmentTest.kt
@@ -0,0 +1,19 @@
+package com.mole.android.mole
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.fragment.app.Fragment
+
+class FragmentTest : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.test_screen, container, false)
+        return view
+    }
+}
--- app/src/main/java/com/mole/android/mole/FragmentWithBotnav.kt
+++ app/src/main/java/com/mole/android/mole/FragmentWithBotnav.kt
@@ -4,23 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.mole.android.mole.ui.navigation_bar.BottomNavigationView
+import androidx.fragment.app.FragmentTransaction
+import com.mole.android.mole.ui.mole_bottom_navigation.MoleBottomNavigation
+import com.mole.android.mole.ui.mole_bottom_navigation.bottom_navigation_bar.BottomNavigationView
 
 class FragmentWithBotnav : Fragment() {
+
+    companion object {
+        const val TAG_1 = "FRAGMENT_1"
+        const val TAG_2 = "FRAGMENT_2"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_with_botnav, container, false)
-        val nav: BottomNavigationView = view.findViewById(R.id.bottomNavigation)
-        nav.menu.getItem(1).isEnabled = false
-        val navController = findNavController(view.findViewById(R.id.nav_host_fragment))
-        nav.setupWithNavController(navController)
 
+        val fragment: TestScreenFragment? =
+            requireActivity().supportFragmentManager.findFragmentByTag(TAG_1) as TestScreenFragment?
+
+        if (fragment == null) {
+            val fragmentTransaction: FragmentTransaction = requireActivity().supportFragmentManager
+                .beginTransaction()
+            fragmentTransaction.add(
+                R.id.nav_host_fragment, TestScreenFragment(), TAG_1
+            )
+            fragmentTransaction.commit()
+        }
+
+        val navigationBar: MoleBottomNavigation = view.findViewById(R.id.navigationCoordinatorLayout)
+
+        navigationBar.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_debts -> {
+                    replaceOnTestScreen(TAG_1, TestScreenFragment())
+                }
+                R.id.navigation_profile -> {
+                    replaceOnTestScreen(TAG_2, FragmentTest())
+                }
+            }
+            true
+        }
         return view
     }
+
+    private fun replaceOnTestScreen(tag: String, fragment: Fragment) {
+        val fragment1: Fragment? =
+            requireActivity().supportFragmentManager.findFragmentByTag(tag)
+
+        if (fragment1 == null) {
+            val fragmentTransaction: FragmentTransaction =
+                requireActivity().supportFragmentManager
+                    .beginTransaction()
+            fragmentTransaction.replace(
+                R.id.nav_host_fragment, fragment, tag
+            )
+            fragmentTransaction.commit()
+        } else {
+            Toast.makeText(
+                requireActivity(), "tag: '$tag' already press",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 }
