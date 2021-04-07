569b58068df4605c460d574e62003af2596908c8
PaladiRka <milakins@bk.ru>
2021-04-07 23:16:57 +0300

    Remove Single Activity class and move his logic to MainActivity

--- app/src/main/java/com/mole/android/mole/MainActivity.kt
+++ app/src/main/java/com/mole/android/mole/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.mole.android.mole
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 
-class MainActivity : SingleFragmentActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun createFragment(): Fragment {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (fragment == null) {
+            val newFragment = createFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, newFragment).commit()
+        }
+    }
+
+    private fun createFragment(): Fragment {
         return FragmentWithBotnav()
-//        return TestScreenFragment()
     }
 }
