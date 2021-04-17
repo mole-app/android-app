0a6e1fbf604d1530634fa1e27150c5aa2706cdef
PaladiRka <milakins@bk.ru>
2021-04-17 23:45:43 +0300

    Fix action bar for messenger.

--- app/src/main/java/com/mole/android/mole/ui/actionbar/MoleActionBarMessenger.kt
+++ app/src/main/java/com/mole/android/mole/ui/actionbar/MoleActionBarMessenger.kt
@@ -2,19 +2,22 @@ package com.mole.android.mole.ui.actionbar
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import com.mole.android.mole.R
 
 class MoleActionBarMessenger @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : Toolbar(context, attrs, defStyleAttr) {
-    init {
-        inflate(context, R.layout.view_action_bar_messenger, this)
-    }
+) : MoleActionBar(context, attrs, defStyleAttr) {
+
     private val nameTextView: TextView = findViewById(R.id.name)
 
     fun setName(name: String){
         nameTextView.text = name
+    }
+
+    override fun customViewId(): Int {
+        return R.layout.view_action_bar_messenger
     }
 }
