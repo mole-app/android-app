8d9abb56098969b59be642586d8d6bcf64dea7b6
PaladiRka <milakins@bk.ru>
2021-04-22 22:00:38 +0300

    Move to separate view xml and restruct MoleMessageView.

--- app/src/main/java/com/mole/android/mole/MoleMessageView.kt
+++ app/src/main/java/com/mole/android/mole/MoleMessageView.kt
@@ -1,15 +1,23 @@
 package com.mole.android.mole
 
 import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.content.res.ColorStateList
-import android.content.res.Resources
+import android.graphics.Point
+import android.graphics.PointF
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
-import android.view.View
+import android.view.*
+import android.view.View.OnLongClickListener
+import android.view.View.OnTouchListener
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
+import com.mole.android.mole.ui.PopupView
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
@@ -18,26 +26,31 @@ class MoleMessageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    init {
-        inflate(context, R.layout.view_message, this)
-        background = ResourcesCompat.getDrawable(resources, R.drawable.shape_massage, null)
-        val paddingEnd = (16 * Resources.getSystem().displayMetrics.density).toInt()
-        val paddingStart = (16 * Resources.getSystem().displayMetrics.density).toInt()
-        val paddingBottom = (16 * Resources.getSystem().displayMetrics.density).toInt()
-        val paddingTop = (16 * Resources.getSystem().displayMetrics.density).toInt()
-        setPaddingRelative(paddingStart, paddingTop, paddingEnd, paddingBottom)
-    }
+    @ColorInt
+    private val colorAccept: Int
 
-    private val tagTextView: TextView = findViewById(R.id.tag)
-    private val timeTextView: TextView = findViewById(R.id.time)
+    @ColorInt
+    private val colorDeny: Int
+
+    @ColorInt
+    private val colorDisabled: Int
+
+    @ColorInt
+    private val colorSelected: Int
+
+    private var postfix: String = ""
+
+    var balance = 0
+        set(value) {
+            updateBalance(value)
+            field = value
+        }
+
     private val balanceTextView: TextView = findViewById(R.id.balance)
 
-
-    @ColorInt private val colorAccept: Int
-    @ColorInt private val colorDeny: Int
-    @ColorInt private val colorDisabled: Int
-
     init {
+        background = ResourcesCompat.getDrawable(resources, R.drawable.shape_massage, null)
+
         val typedValue = TypedValue()
         val theme = context.theme
         theme.resolveAttribute(R.attr.colorAccept, typedValue, true)
@@ -46,77 +59,28 @@ class MoleMessageView @JvmOverloads constructor(
         colorDisabled = typedValue.data
         theme.resolveAttribute(R.attr.colorDeny, typedValue, true)
         colorDeny = typedValue.data
-    }
+        theme.resolveAttribute(R.attr.colorAccent, typedValue, true)
+        colorSelected = typedValue.data
 
-    private var postfix: String = ""
-
-    var balance = 0
-        set(value) {
-            val sign: String
-            when (value.sign) {
-                1 -> {
-                    sign = "+ "
-                    val states = arrayOf(
-                        intArrayOf(android.R.attr.state_enabled),
-                        intArrayOf(-android.R.attr.state_enabled)
-                    )
-                    val colors = intArrayOf(
-                        colorAccept,
-                        colorAccept
-                    )
-                    val myList = ColorStateList(states, colors)
-                    backgroundTintList = myList
-                }
-                -1 -> {
-                    sign = "- "
-                    val states = arrayOf(
-                        intArrayOf(android.R.attr.state_enabled),
-                        intArrayOf(-android.R.attr.state_enabled)
-                    )
-                    val colors = intArrayOf(
-                        colorDeny,
-                        colorDeny
-                    )
-                    val myList = ColorStateList(states, colors)
-                    backgroundTintList = myList
-                }
-                else -> {
-                    sign = ""
-                    val states = arrayOf(
-                        intArrayOf(android.R.attr.state_enabled),
-                        intArrayOf(-android.R.attr.state_enabled)
-                    )
-                    val colors = intArrayOf(
-                        colorDisabled,
-                        colorDisabled
-                    )
-                    val myList = ColorStateList(states, colors)
-                    backgroundTintList = myList
-                }
-            }
-
-            balanceTextView.text = "$sign${value.absoluteValue} $postfix"
-            field = value
-        }
-
-    init {
         init(context, attrs)
-        balance = 1500
+
+        balanceTextView.text = "${0}"
+        balance = 15000
     }
 
     private fun init(context: Context, attrs: AttributeSet?) {
         if (attrs != null) {
             val typedArray = context.obtainStyledAttributes(
                 attrs,
-                R.styleable.MoleMessage,
+                R.styleable.MoleMessageView,
                 0,
                 0
             )
             val (additionalInfo, postfix) = try {
                 val additionalInfo =
-                    typedArray.getInt(R.styleable.MoleMessage_additional_info, 0)
+                    typedArray.getInt(R.styleable.MoleMessageView_additional_info, 0)
 
-                val postfix = typedArray.getString(R.styleable.MoleMessage_postfix)
+                val postfix = typedArray.getString(R.styleable.MoleMessageView_postfix)
 
                 additionalInfo to postfix
             } finally {
@@ -140,28 +104,60 @@ class MoleMessageView @JvmOverloads constructor(
     }
 
     private fun setAdditionalInfoAttr(additionalInfo: Int) {
-
         when (additionalInfo) {
-            0 -> {
-                tagTextView.visibility = View.VISIBLE
-                timeTextView.visibility = View.VISIBLE
-                val paddingEnd = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingStart = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingBottom = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingTop = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                setPaddingRelative(paddingStart, paddingTop, paddingEnd, paddingBottom)
-
-            }
             1 -> {
-                tagTextView.visibility = View.GONE
-                timeTextView.visibility = View.GONE
-                val paddingEnd = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingStart = (16 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingBottom = (8 * Resources.getSystem().displayMetrics.density).toInt()
-                val paddingTop = (8 * Resources.getSystem().displayMetrics.density).toInt()
-                setPaddingRelative(paddingStart, paddingTop, paddingEnd, paddingBottom)
+                inflate(context, R.layout.view_message, this)
+            }
+            0 -> {
+                inflate(context, R.layout.view_message_with_info, this)
             }
         }
+    }
+
+    private fun updateBalance(value: Int) {
+        val sign: String
+        when (value.sign) {
+            1 -> {
+                sign = "+ "
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_enabled),
+                    intArrayOf(-android.R.attr.state_enabled)
+                )
+                val colors = intArrayOf(
+                    colorAccept,
+                    colorAccept
+                )
+                val myList = ColorStateList(states, colors)
+                backgroundTintList = myList
+            }
+            -1 -> {
+                sign = "- "
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_enabled),
+                    intArrayOf(-android.R.attr.state_enabled)
+                )
+                val colors = intArrayOf(
+                    colorDeny,
+                    colorDeny
+                )
+                val myList = ColorStateList(states, colors)
+                backgroundTintList = myList
+            }
+            else -> {
+                sign = ""
+                val states = arrayOf(
+                    intArrayOf(android.R.attr.state_enabled),
+                    intArrayOf(-android.R.attr.state_enabled)
+                )
+                val colors = intArrayOf(
+                    colorDisabled,
+                    colorDisabled
+                )
+                val myList = ColorStateList(states, colors)
+                backgroundTintList = myList
+            }
+        }
+        balanceTextView.text = "$sign${value.absoluteValue}$postfix"
     }
 
 }
