b804bf0bb12576729d6102ead0c1ada3e0d4184b
PaladiRka <milakins@bk.ru>
2021-05-03 19:45:56 +0300

    Add popupView and darkView with select

---
+++ app/src/main/java/com/mole/android/mole/ui/DarkView.kt
@@ -0,0 +1,61 @@
+package com.mole.android.mole.ui
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.annotation.ColorInt
+
+class DarkView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    @ColorInt
+    var colorSelected: Int = 0
+
+    var lastColorView: ColorStateList? = null
+
+    private val rect = Rect()
+    var selectedView: View? = null
+        set(value) {
+            value?.getGlobalVisibleRect(rect)
+            Log.i(
+                "DarkView",
+                "rect top ${rect.top} bottom ${rect.bottom} left ${rect.left} right ${rect.right}"
+            )
+            field = value
+        }
+    private val location = IntArray(2)
+
+    init {
+        setBackgroundColor(Color.parseColor("#A0000000"))
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        selectedView?.apply {
+            getLocationOnScreen(location)
+            getWindowVisibleDisplayFrame(rect)
+
+            canvas?.clipRect(rect)
+            canvas?.translate(location[0].toFloat(), location[1].toFloat())
+
+            lastColorView = selectedView?.backgroundTintList
+
+            if (colorSelected != 0) {
+                backgroundTintList = ColorStateList.valueOf(colorSelected)
+            }
+
+            draw(canvas)
+        }
+    }
+
+    fun dismiss() {
+        if (colorSelected != 0) {
+            selectedView?.backgroundTintList = lastColorView
+        }
+    }
+}
---
+++ app/src/main/java/com/mole/android/mole/ui/PopupView.kt
@@ -0,0 +1,106 @@
+package com.mole.android.mole.ui
+
+import android.content.Context
+import android.graphics.PixelFormat
+import android.graphics.Rect
+import android.os.IBinder
+import android.util.DisplayMetrics
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import android.widget.FrameLayout
+import android.widget.PopupWindow
+
+
+class PopupView constructor(
+    contentView: View, width: Int, height: Int, focusable: Boolean = true
+) : PopupWindow(contentView, width, height, focusable) {
+
+    private var isDarkInvoked: Boolean = false
+    private val darkView: DarkView?
+    private val windowManager: WindowManager?
+
+    interface OnDismissListener {
+        fun onDismiss()
+    }
+
+    var onDismissListener: OnDismissListener? = null
+
+    var colorSelected: Int
+        set(value) {
+            darkView?.colorSelected = value
+        }
+        get() = darkView?.colorSelected ?: 0
+
+    var selectedView: View?
+        set(value) {
+            val rect = Rect()
+            value?.getGlobalVisibleRect(rect)
+            darkView?.selectedView = value
+        }
+        get() = darkView?.selectedView
+
+    init {
+        windowManager =
+            contentView.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        darkView = DarkView(contentView.context)
+        darkView.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        )
+
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+
+    }
+
+    private fun createDarkLayout(token: IBinder): WindowManager.LayoutParams {
+        val p = WindowManager.LayoutParams()
+        p.gravity = Gravity.START or Gravity.TOP
+        p.width = WindowManager.LayoutParams.MATCH_PARENT
+        p.height = WindowManager.LayoutParams.MATCH_PARENT
+        p.format = PixelFormat.TRANSLUCENT
+        p.type = WindowManager.LayoutParams.LAST_SUB_WINDOW
+        p.token = token
+        p.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED
+        return p
+    }
+
+    override fun showAsDropDown(anchor: View?, xoff: Int, yoff: Int) {
+        invokeBgCover(anchor!!)
+        super.showAsDropDown(anchor, xoff, yoff)
+    }
+
+    override fun showAtLocation(parent: View?, gravity: Int, x: Int, y: Int) {
+        invokeBgCover(parent!!)
+        super.showAtLocation(parent, gravity, x, y)
+    }
+
+    private fun invokeBgCover(view: View) {
+        if (isDarkInvoked || isShowing || contentView == null) {
+            return
+        }
+        if (darkView != null) {
+            val darkLP = createDarkLayout(view.windowToken)
+            windowManager?.addView(darkView, darkLP)
+            isDarkInvoked = true
+        }
+    }
+
+    override fun dismiss() {
+        super.dismiss()
+        if (darkView != null && isDarkInvoked) {
+            darkView.dismiss()
+            windowManager?.removeViewImmediate(darkView)
+            isDarkInvoked = false
+            darkView.selectedView?.invalidate()
+        }
+        onDismissListener?.onDismiss()
+    }
+
+    companion object {
+        class Builder {
+            
+        }
+    }
+}
