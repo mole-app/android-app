465d4c22943ab403156235732164b2eca1e9f07b
PaladiRka <milakins@bk.ru>
2021-05-06 10:13:31 +0300

    Fix alert size.

--- app/src/main/java/com/mole/android/mole/MoleAlertDialog.kt
+++ app/src/main/java/com/mole/android/mole/MoleAlertDialog.kt
@@ -1,12 +1,10 @@
 package com.mole.android.mole
 
 import android.app.Dialog
-import android.graphics.drawable.ColorDrawable
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
+import android.view.*
 import androidx.fragment.app.DialogFragment
 import com.mole.android.mole.ui.BlurView
 
@@ -14,36 +12,32 @@ import com.mole.android.mole.ui.BlurView
 class MoleAlertDialog : DialogFragment() {
     var rootView: ViewGroup? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        val view = inflater.inflate(R.layout.dialog_alert, container, false)
-        val blurView: BlurView = view.findViewById(R.id.dialog_root)
-        blurView.setupWith(rootView!!).setBlurRadius(12f)
-        blurView.cornerRadius(8f.dp())
-
-        return view
-//        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(
-            requireActivity()
-        )
         val view: View = LayoutInflater.from(context).inflate(R.layout.dialog_alert, null)
         val blurView: BlurView = view.findViewById(R.id.dialog_root)
         blurView.setupWith(rootView!!).setBlurRadius(12f)
         blurView.cornerRadius(8f.dp())
 
-        builder.setView(view)
-        val alertDialog = builder.create()
-        if (alertDialog.window != null) {
-            alertDialog.window!!.setBackgroundDrawable(ColorDrawable(0))
-        }
+        val alertDialog = Dialog(requireActivity(), R.style.MoleDialog)
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        alertDialog.setContentView(blurView)
         alertDialog.setCancelable(true)
+        alertDialog.setCanceledOnTouchOutside(true)
+
+        val fragmentActivity = requireActivity()
+        val wm = fragmentActivity.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+
+        val display = wm.defaultDisplay
+        val size = Point()
+        display?.getSize(size)
+        val width = size.x - (24.dp() * 2) // margin 24dp for window on both sides
+
+        val layoutParams = WindowManager.LayoutParams()
+        layoutParams.copyFrom(alertDialog.window?.attributes)
+        layoutParams.width = width
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
+        alertDialog.window?.attributes = layoutParams
         return alertDialog
     }
 }
