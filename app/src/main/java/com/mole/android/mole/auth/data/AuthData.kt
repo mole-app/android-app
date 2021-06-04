40047520b18df7f8b4109bb8087d18235518abea
PaladiRka <milakins@bk.ru>
2021-06-04 22:44:39 +0300

    AuthData to data class.

--- app/src/main/java/com/mole/android/mole/auth/data/AuthData.kt
+++ app/src/main/java/com/mole/android/mole/auth/data/AuthData.kt
@@ -1,13 +1,8 @@
 package com.mole.android.mole.auth.data
 
-class AuthData(
+data class AuthData(
     val login: String,
 ) {
-
-    fun copy(
-        login: String = this.login
-    ) = AuthData(login)
-
     fun setLogin(login: String): AuthData {
         return this.copy(login = login)
     }
