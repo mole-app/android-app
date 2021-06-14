4d55576427f86cd8ebbf30aa11b5a473ea41be57
PaladiRka <milakins@bk.ru>
2021-06-14 12:25:19 +0300

    Add testing functionality.

--- app/src/main/java/com/mole/android/mole/test/TestScreenFragment.kt
+++ app/src/main/java/com/mole/android/mole/test/TestScreenFragment.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
 import com.mole.android.mole.R
+import com.mole.android.mole.Shape
+import com.mole.android.mole.dp
+import com.mole.android.mole.setBorder
 import com.mole.android.mole.ui.actionbar.MoleActionBar
 
 
@@ -21,7 +24,16 @@ class TestScreenFragment : Fragment() {
         val view = inflater.inflate(R.layout.test_screen, container, false)
         val circleImageButton: AppCompatImageButton =
             view.findViewById(R.id.test_screen_circle_button_disable)
-        circleImageButton.isEnabled = false
+        circleImageButton.setBorder(
+            Shape.OVAL,
+            80f.dp(),
+            1f.dp(),
+            R.attr.colorIconDisabled,
+            R.attr.colorGradientStroke
+        )
+        circleImageButton.setOnClickListener {
+            it.isEnabled = false
+        }
 
         toolbar = view.findViewById(R.id.mole_toolbar_with_text)
         val appCompatActivity = requireActivity()
