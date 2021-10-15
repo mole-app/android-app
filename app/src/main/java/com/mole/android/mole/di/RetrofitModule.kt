3d853748b9c9bbc13f54cb77600ad9c76ad84061
PaladiRka <milakins@bk.ru>
2021-10-15 08:53:24 +0300

    Move RetrofitBuilder to web service package.

--- app/src/main/java/com/mole/android/mole/di/RetrofitModule.kt
+++ app/src/main/java/com/mole/android/mole/di/RetrofitModule.kt
@@ -1,6 +1,6 @@
 package com.mole.android.mole.di
 
-import com.mole.android.mole.auth.model.RetrofitBuilder
+import com.mole.android.mole.web.service.RetrofitBuilder
 
 class RetrofitModule: Module() {
     val retrofit by lazy { RetrofitBuilder.build() }
--- app/src/main/java/com/mole/android/mole/web/service/RetrofitBuilder.kt
+++ app/src/main/java/com/mole/android/mole/web/service/RetrofitBuilder.kt
@@ -1,4 +1,4 @@
-package com.mole.android.mole.auth.model
+package com.mole.android.mole.web.service
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
