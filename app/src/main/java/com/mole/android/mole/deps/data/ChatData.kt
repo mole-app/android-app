108c3c3be80a86c074ce21d1e2f6987e554f22ef
omy-q <olya_aparina@mail.ru>
2021-11-03 18:58:25 +0300

    add test data

---
+++ app/src/main/java/com/mole/android/mole/deps/data/ChatData.kt
@@ -0,0 +1,17 @@
+package com.mole.android.mole.deps.data
+
+data class ChatData(
+    val userIcon : String,
+    val userName : String,
+    val userDebtsCount : String,
+    val userTotalDebts : Int
+)
+
+val testData = listOf<ChatData>(
+    ChatData("0", "Test", "Test", +0),
+    ChatData("1", "Александр", "10 долгов", +1500),
+    ChatData("2", "Андрей", "1 долг", -500),
+    ChatData("3", "Анастасия", "2 долга", +100),
+)
+
+
