6ee0fad8d2a945e20d725c49239979e98dabf8f2
aleksandr.milakin <aleksandr.milakin@myoffice.team>
2022-03-07 11:28:21 +0300

    Api calls pretty.

--- app/src/main/java/com/mole/android/mole/auth/data/AuthDataLogin.kt
+++ app/src/main/java/com/mole/android/mole/auth/data/AuthDataLogin.kt
@@ -4,5 +4,5 @@ data class AuthDataLogin(
     val accessToken: String,
     val refreshToken: String,
     val expiresIn: String,
-    val login: String
+    val login: String?
 )
--- app/src/main/java/com/mole/android/mole/auth/model/AuthModelImplementation.kt
+++ app/src/main/java/com/mole/android/mole/auth/model/AuthModelImplementation.kt
@@ -6,6 +6,7 @@ import com.mole.android.mole.component
 import kotlinx.coroutines.*
 
 import com.mole.android.mole.di.FirebaseModule
+import com.mole.android.mole.web.service.ApiResult
 
 
 class AuthModelImplementation(
@@ -26,48 +27,52 @@ class AuthModelImplementation(
         return login != "first"
     }
 
-    override suspend fun getUserVk(code: String): String {
+    override suspend fun getUserVk(code: String): ApiResult<AuthModel.SuccessAuthResult> {
         val task = mainScope.async(Dispatchers.IO) {
             try {
-                val user = service.getVkAuth(code, getFingerprint())
-                val accountRepository = component().accountManagerModule.accountRepository
-                val success = accountRepository.createAccount(
-                    user.login ?: "VovchikPut",
-                    user.accessToken,
-                    user.refreshToken
-                )
-                user
+                val user = service.getVkAuth(code + "1234", getFingerprint())
+                if (user.login == null) {
+                    ApiResult.create<AuthModel.SuccessAuthResult>(AuthModel.SuccessAuthResult.SuccessForExistedUser)
+                } else {
+                    val accountRepository = component().accountManagerModule.accountRepository
+                    val success = accountRepository.createAccount(
+                        user.login,
+                        user.accessToken,
+                        user.refreshToken
+                    )
+                    ApiResult.create<AuthModel.SuccessAuthResult>(AuthModel.SuccessAuthResult.SuccessNewUser(user.login))
+                }
             } catch (exception: Exception) {
                 // Не хочется падать если что-то не так на сервере
                 exception.printStackTrace()
-                data
+                ApiResult.create(ApiResult.MoleError(100))
             }
         }
-        val user: AuthDataLogin = task.await()
-        Log.i("Auth", "User vk login: $user")
-        return user.login
+        return task.await()
     }
 
-    override suspend fun getUserGoogle(code: String): String {
+    override suspend fun getUserGoogle(code: String): ApiResult<AuthModel.SuccessAuthResult> {
         val task = mainScope.async(Dispatchers.IO) {
             try {
                 val user = service.getGoogleAuth(code, getFingerprint())
-                val accountRepository = component().accountManagerModule.accountRepository
-                val success = accountRepository.createAccount(
-                    user.login ?: "VovchikPut",
-                    user.accessToken,
-                    user.refreshToken
-                )
-                user
+                if (user.login == null) {
+                    ApiResult.create<AuthModel.SuccessAuthResult>(AuthModel.SuccessAuthResult.SuccessForExistedUser)
+                } else {
+                    val accountRepository = component().accountManagerModule.accountRepository
+                    val success = accountRepository.createAccount(
+                        user.login,
+                        user.accessToken,
+                        user.refreshToken
+                    )
+                    ApiResult.create<AuthModel.SuccessAuthResult>(AuthModel.SuccessAuthResult.SuccessNewUser(user.login))
+                }
             } catch (exception: Exception) {
                 // Не хочется падать если что-то не так на сервере
                 exception.printStackTrace()
-                data
+                ApiResult.create(ApiResult.MoleError(100))
             }
         }
-        val user = task.await()
-        Log.i("Auth", "User google login: ${user.login}")
-        return user.login
+        return task.await()
     }
 
     private suspend fun getFingerprint(): String {
