1d016784276e534da1e32af46600a273bec0eac6
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2022-03-11 19:31:18 +0300

    add layout for choose side screen

--- app/src/main/java/com/mole/android/mole/create/view/ChooseSideScreen.kt
+++ app/src/main/java/com/mole/android/mole/create/view/ChooseSideScreen.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.View
 import com.mole.android.mole.MoleBaseFragment
 import com.mole.android.mole.databinding.FragmentChooseSideBinding
+import com.mole.android.mole.ui.actionbar.MoleActionBar
 
 class ChooseSideScreen : MoleBaseFragment<FragmentChooseSideBinding>(FragmentChooseSideBinding::inflate) {
+
+    override fun getToolbar(): MoleActionBar {
+        return binding.actionBar
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
