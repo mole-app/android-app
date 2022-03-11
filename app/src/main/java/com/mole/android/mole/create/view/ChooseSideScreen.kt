25626f28b4be927bafa59cc908fb51462e77c7f0
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2022-03-11 19:16:53 +0300

    fix card design

--- app/src/main/java/com/mole/android/mole/create/view/ChooseSideScreen.kt
+++ app/src/main/java/com/mole/android/mole/create/view/ChooseSideScreen.kt
@@ -1,7 +1,16 @@
 package com.mole.android.mole.create.view
 
+import android.os.Bundle
+import android.view.View
 import com.mole.android.mole.MoleBaseFragment
 import com.mole.android.mole.databinding.FragmentChooseSideBinding
 
 class ChooseSideScreen : MoleBaseFragment<FragmentChooseSideBinding>(FragmentChooseSideBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.createDebtPositiveButton.render(ChooseSideCardView.Side.HIS_DEBT)
+        binding.createDebtNegativeButton.render(ChooseSideCardView.Side.MY_DEBT)
+    }
+
 }
