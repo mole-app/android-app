7650317dc6054b78f3967f2efedbe29e98aa6fc2
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2022-03-13 14:37:28 +0300

    add tab mediator

--- app/src/main/java/com/mole/android/mole/create/view/steps/CreateStepsScreen.kt
+++ app/src/main/java/com/mole/android/mole/create/view/steps/CreateStepsScreen.kt
@@ -2,6 +2,7 @@ package com.mole.android.mole.create.view.steps
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mole.android.mole.MoleBaseFragment
 import com.mole.android.mole.databinding.FragmentCreateStepsBinding
 import com.mole.android.mole.ui.actionbar.MoleActionBar
@@ -14,5 +15,12 @@ class CreateStepsScreen : MoleBaseFragment<FragmentCreateStepsBinding>(FragmentC
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val adapter = StepsAdapter(
+            listOf(Steps.ChooseName, Steps.ChooseTag, Steps.ChooseAmount)
+        )
+        binding.viewPager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        }.attach()
+        adapter.notifyDataSetChanged()
     }
 }
