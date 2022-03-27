4d17f6483919e9f9f898339a264be145dba28148
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2022-03-27 13:56:43 +0300

    fix bug with animation

--- app/src/main/java/com/mole/android/mole/create/view/name/ChooseNameViewHolder.kt
+++ app/src/main/java/com/mole/android/mole/create/view/name/ChooseNameViewHolder.kt
@@ -1,24 +1,16 @@
 package com.mole.android.mole.create.view.name
 
 import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.TextView
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.transition.TransitionManager
 import com.google.android.material.textfield.TextInputLayout
 import com.mole.android.mole.R
-import com.mole.android.mole.auth.view.MoleTextInputLayout
 import com.mole.android.mole.create.view.steps.BaseStepsHolder
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.constraintlayout.widget.ConstraintSet
 
-
-
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class ChooseNameViewHolder(parent: ViewGroup) : BaseStepsHolder(parent, R.layout.holder_choose_name) {
 
@@ -30,11 +22,21 @@ class ChooseNameViewHolder(parent: ViewGroup) : BaseStepsHolder(parent, R.layout
         val text = loginContainer.findViewById<TextInputLayout>(R.id.text)
         val clickableArea = loginContainer.findViewById<View>(R.id.clickable_area)
         listContainer.setOnClickListener {
-            (itemView as? ViewGroup)?.let { viewGroup ->
+            (itemView as? ConstraintLayout)?.let { viewGroup ->
                 if (!mode) {
                     mode = true
                     TransitionManager.beginDelayedTransition(viewGroup)
-                    listContainer.visibility = View.GONE
+                    val constraintLayout = viewGroup
+                    val constraintSet = ConstraintSet()
+                    constraintSet.clone(constraintLayout)
+                    constraintSet.connect(
+                        listContainer.id,
+                        ConstraintSet.TOP,
+                        ConstraintSet.PARENT_ID,
+                        ConstraintSet.BOTTOM,
+                        0
+                    )
+                    constraintSet.applyTo(constraintLayout)
                     clickableArea.visibility = View.VISIBLE
                     val imm = itemView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
                     imm!!.hideSoftInputFromWindow(itemView.getWindowToken(), 0)
@@ -43,11 +45,23 @@ class ChooseNameViewHolder(parent: ViewGroup) : BaseStepsHolder(parent, R.layout
         }
 
         clickableArea.setOnClickListener {
-            (itemView as? ViewGroup)?.let { viewGroup ->
+            (itemView as? ConstraintLayout)?.let { viewGroup ->
                 if (mode) {
                     mode = false
                     TransitionManager.beginDelayedTransition(viewGroup)
-                    listContainer.visibility = View.VISIBLE
+
+                    val constraintLayout = viewGroup
+                    val constraintSet = ConstraintSet()
+                    constraintSet.clone(constraintLayout)
+                    constraintSet.connect(
+                        listContainer.id,
+                        ConstraintSet.TOP,
+                        loginContainer.id,
+                        ConstraintSet.BOTTOM,
+                        0
+                    )
+                    constraintSet.applyTo(constraintLayout)
+
                     clickableArea.visibility = View.INVISIBLE
                     val imm = itemView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_IMPLICIT_ONLY)
