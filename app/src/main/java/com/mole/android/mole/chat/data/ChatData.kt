23d9d84327da6da6a28015442ca1f3670a4b1d84
omy-q <olya_aparina@mail.ru>
2022-03-31 14:32:59 +0300

    add test data

--- app/src/main/java/com/mole/android/mole/chat/data/ChatData.kt
+++ app/src/main/java/com/mole/android/mole/chat/data/ChatData.kt
@@ -14,13 +14,24 @@ sealed class ChatData {
 }
 
 val testChatData = mutableListOf<ChatData>(
-    ChatData.ChatDate("26.03.2022"),
-    ChatData.ChatMessage(true, -150),
     ChatData.ChatMessage(false, +1000),
-    ChatData.ChatMessage(false, -800),
-    ChatData.ChatDate("27.03.2022"),
+    ChatData.ChatDate("03.04.2022"),
     ChatData.ChatMessage(false, -1000),
+    ChatData.ChatDate("02.04.2022"),
+    ChatData.ChatMessage(false, +1000),
+    ChatData.ChatDate("01.04.2022"),
+    ChatData.ChatMessage(false, -1000),
+    ChatData.ChatDate("31.03.2022"),
+    ChatData.ChatMessage(false, +1000),
+    ChatData.ChatDate("30.03.2022"),
+    ChatData.ChatMessage(false, -1000),
+    ChatData.ChatDate("29.03.2022"),
     ChatData.ChatMessage(true, +1800),
     ChatData.ChatMessage(true, +150),
-    ChatData.ChatDate("29.03.2022")
+    ChatData.ChatDate("27.03.2022"),
+    ChatData.ChatMessage(false, -800),
+    ChatData.ChatMessage(false, +1000),
+    ChatData.ChatMessage(true, -150),
+    ChatData.ChatDate("26.03.2022"),
+
 )
