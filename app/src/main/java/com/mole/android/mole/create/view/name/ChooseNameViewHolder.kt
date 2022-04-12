4080e70ac108d3a56815ccf8696d3b83c0a8d4fd
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2022-04-12 21:33:39 +0300

    implement diff contract

--- app/src/main/java/com/mole/android/mole/create/view/name/ChooseNameViewHolder.kt
+++ app/src/main/java/com/mole/android/mole/create/view/name/ChooseNameViewHolder.kt
@@ -9,6 +9,7 @@ import coil.transform.CircleCropTransformation
 import com.mole.android.mole.R
 import com.mole.android.mole.create.model.UserPreview
 import com.mole.android.mole.create.view.ChooseTextItemView
+import com.mole.android.mole.create.view.DiffFindItemsViewContract
 import com.mole.android.mole.create.view.steps.BaseStepsHolder
 import com.mole.android.mole.profile.data.ProfilePhoto
 
@@ -27,20 +28,29 @@ class ChooseNameViewHolder(parent: ViewGroup, private val nextClickedListener: (
             )
         }
         (itemView as? ChooseTextItemView)?.let { chooseItemView ->
-            chooseItemView.setDataBinder(
-                object : ChooseTextItemView.ItemViewContract {
-                    override val layoutId: Int = R.layout.choose_user_holder
-                    override val titleId: Int = R.string.choose_login_title
-                    override fun itemsCount(): Int = data.size
-                    override fun bind(view: View, position: Int) = bindView(view, data[position])
-                    override fun contentSame(firstPosition: Int, secondPosition: Int) = false
-                    override fun itemSame(firstPosition: Int, secondPosition: Int) = false
-                    override fun textForClickedItem(position: Int) = data[position].login
-                    override fun onNextClicked() = nextClickedListener()
-                    override fun onTextChanged(text: String) {}
-                }
-            )
+            val contract = object : DiffFindItemsViewContract<UserPreview>(
+                chooseItemView,
+                this@ChooseNameViewHolder::itemsSame,
+                this@ChooseNameViewHolder::contentSame,
+                this@ChooseNameViewHolder::bindView,
+                UserPreview::login
+            ) {
+                override val layoutId: Int = R.layout.choose_user_holder
+                override val titleId: Int = R.string.choose_login_title
+                override fun onNextClicked() = nextClickedListener()
+                override fun onTextChanged(text: String) {}
+            }
+            chooseItemView.setDataBinder(contract)
+            contract.updateData(data)
         }
+    }
+
+    private fun itemsSame(first: UserPreview, second: UserPreview): Boolean {
+        return first.id == second.id
+    }
+
+    private fun contentSame(first: UserPreview, second: UserPreview): Boolean {
+        return first == second
     }
 
     fun bindView(view: View, item: UserPreview) {
--- app/src/main/java/com/mole/android/mole/create/view/tag/ChooseTagHolder.kt
+++ app/src/main/java/com/mole/android/mole/create/view/tag/ChooseTagHolder.kt
@@ -1,12 +1,12 @@
 package com.mole.android.mole.create.view.tag
 
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.mole.android.mole.R
 import com.mole.android.mole.create.model.TagPreview
 import com.mole.android.mole.create.view.ChooseTextItemView
+import com.mole.android.mole.create.view.DiffFindItemsViewContract
 import com.mole.android.mole.create.view.steps.BaseStepsHolder
 
 class ChooseTagHolder(parent: ViewGroup, private val nextClickedListener: () -> Unit) : BaseStepsHolder(parent, R.layout.holder_choose_tag) {
@@ -19,24 +19,32 @@ class ChooseTagHolder(parent: ViewGroup, private val nextClickedListener: () -> 
             )
         }
         (itemView as? ChooseTextItemView)?.let { chooseItemView ->
-            chooseItemView.setDataBinder(
-                object : ChooseTextItemView.ItemViewContract {
-                    override val layoutId: Int = R.layout.choose_tag_item_holder
-                    override val titleId: Int = R.string.choose_tag_title
-                    override fun itemsCount(): Int = data.size
-                    override fun bind(view: View, position: Int) = bindView(view, data[position])
-                    override fun contentSame(firstPosition: Int, secondPosition: Int) = false
-                    override fun itemSame(firstPosition: Int, secondPosition: Int) = false
-                    override fun textForClickedItem(position: Int) = data[position].name
-                    override fun onNextClicked() = nextClickedListener()
-                    override fun onTextChanged(text: String) {
-                    }
-                }
-            )
+            val contract = object : DiffFindItemsViewContract<TagPreview>(
+                chooseItemView,
+                this@ChooseTagHolder::itemsSame,
+                this@ChooseTagHolder::contentSame,
+                this@ChooseTagHolder::bindView,
+                TagPreview::name
+            ) {
+                override val layoutId: Int = R.layout.choose_tag_item_holder
+                override val titleId: Int = R.string.choose_tag_title
+                override fun onNextClicked() = nextClickedListener()
+                override fun onTextChanged(text: String) {}
+            }
+            chooseItemView.setDataBinder(contract)
+            contract.updateData(data)
         }
     }
 
-    fun bindView(view: View, item: TagPreview) {
+    private fun itemsSame(first: TagPreview, second: TagPreview): Boolean {
+        return first.name == second.name
+    }
+
+    private fun contentSame(first: TagPreview, second: TagPreview): Boolean {
+        return first == second
+    }
+
+    private fun bindView(view: View, item: TagPreview) {
         val tag = view.findViewById<TextView>(R.id.tag_name)
         val count = view.findViewById<TextView>(R.id.tag_count)
 
