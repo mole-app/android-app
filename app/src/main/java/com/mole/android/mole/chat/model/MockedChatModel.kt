d6e337e198407acc8a2c6f7aa749d24657cd9feb
omy-q <olya_aparina@mail.ru>
2022-06-17 20:57:03 +0300

    create mocked data

---
+++ app/src/main/java/com/mole/android/mole/chat/model/MockedChatModel.kt
@@ -0,0 +1,28 @@
+package com.mole.android.mole.chat.model
+
+import com.mole.android.mole.chat.data.testChatData
+import com.mole.android.mole.web.service.ApiResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import retrofit2.HttpException
+
+class MockedChatModel(
+    private val mainScope: CoroutineScope
+) : ChatModel {
+    override suspend fun loadNextData(leftCountData: Int): ApiResult<ChatModel.SuccessChatResult> {
+        val task = mainScope.async(Dispatchers.IO) {
+            try {
+                Thread.sleep(1000)
+                ApiResult.create<ChatModel.SuccessChatResult>(
+                    ChatModel.SuccessChatResult.DataBatch(
+                        testChatData
+                    )
+                )
+            } catch (exception: HttpException) {
+                ApiResult.create(ApiResult.MoleError(exception.code(), exception.message()))
+            }
+        }
+        return task.await()
+    }
+}
