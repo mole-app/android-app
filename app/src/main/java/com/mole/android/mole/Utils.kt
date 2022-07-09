17d2af604b724430952b6ddb2627f785c40d502e
omy-q <olya_aparina@mail.ru>
2022-07-09 20:26:23 +0300

    set time zone

--- app/src/main/java/com/mole/android/mole/Utils.kt
+++ app/src/main/java/com/mole/android/mole/Utils.kt
@@ -40,18 +40,17 @@ fun tagsToString(tags: List<String>): String {
 
 fun stringToDate(date: String): Date{
     val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.ROOT)
+    formatter.timeZone = TimeZone.getTimeZone("GMT")
     return formatter.parse(date) ?: Date()
 }
 
 fun dateToString(date: Date): String {
     val formatter = SimpleDateFormat("dd.MM.yyyy", Locale.ROOT)
-    formatter.timeZone = TimeZone.getDefault()
     return formatter.format(date)
 }
 
 fun timeToString(time : Date): String {
     val formatter = SimpleDateFormat("HH:mm", Locale.ROOT)
-    formatter.timeZone = TimeZone.getDefault()
     return formatter.format(time)
 }
 
