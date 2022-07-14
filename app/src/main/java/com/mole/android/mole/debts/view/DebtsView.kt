b49f2bf882b8267da23854cbd8a7ecb4e1157b7a
omy-q <olya_aparina@mail.ru>
2022-07-14 13:35:43 +0300

    refactor view and presentation level -> add view methods

--- app/src/main/java/com/mole/android/mole/debts/view/DebtsView.kt
+++ app/src/main/java/com/mole/android/mole/debts/view/DebtsView.kt
@@ -5,4 +5,9 @@ import com.mole.android.mole.debts.data.DebtsData
 
 interface DebtsView : MoleBaseView {
     fun setData(data: List<DebtsData>)
+    fun showLoading()
+    fun hideLoading()
+    fun showError(code: Int, description: String)
+    fun showDeleteDialog()
+    fun showChatScreen(idDebtor: Int)
 }
--- app/src/main/java/com/mole/android/mole/debts/view/DebtsViewImplementation.kt
+++ app/src/main/java/com/mole/android/mole/debts/view/DebtsViewImplementation.kt
@@ -2,42 +2,32 @@ package com.mole.android.mole.debts.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.mole.android.mole.MoleBaseFragment
 import com.mole.android.mole.R
 import com.mole.android.mole.component
-import com.mole.android.mole.databinding.FragmentDebtsMainBinding
+import com.mole.android.mole.databinding.FragmentDebtsBinding
 import com.mole.android.mole.debts.data.DebtsData
 import com.mole.android.mole.navigation.Screens
 
 class DebtsViewImplementation :
-    MoleBaseFragment<FragmentDebtsMainBinding>(FragmentDebtsMainBinding::inflate), DebtsView {
+    MoleBaseFragment<FragmentDebtsBinding>(FragmentDebtsBinding::inflate), DebtsView {
 
     private val presenter = component().debtsModule.debtsPresenter
-    override fun getNavigator() = AppNavigator(requireActivity(), R.id.nav_host_fragment)
-
-    private val adapter = DebtsViewAdapter(object : OnItemChatClickListener {
+    private val listener = object : OnItemDebtsClickListener {
         override fun onLongClick(view: View, chatData: DebtsData.ChatDebtsData) {
-            presenter.onLongChatClick()
+            presenter.onItemLongClick()
         }
 
         override fun onShotClick(chatData: DebtsData.ChatDebtsData) {
-            component().routingModule.navigationHolder.setNavigator(
-                AppNavigator(
-                    requireActivity(),
-                    R.id.fragment_container
-                )
-            )
-            component().routingModule.router.navigateTo(
-                Screens.Chat(
-                    name = chatData.personName,
-                    totalDebts = chatData.personDebtsTotal,
-                    avatarUrl = chatData.personIcon
-                )
-            )
+            presenter.onItemShortClick(chatData.id)
         }
-    })
+    }
+    private val debtsAdapter = DebtsAdapter(listener)
+
+    override fun getNavigator() = AppNavigator(requireActivity(), R.id.nav_host_fragment)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -46,11 +36,11 @@ class DebtsViewImplementation :
     }
 
     private fun initRecyclerView() {
-        binding.DebtsRecyclerView.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        binding.DebtsRecyclerView.adapter = adapter
-        adapter.setData(presenter.getData())
-        adapter.notifyDataSetChanged()
+        with(binding.DebtsRecyclerView) {
+            layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = debtsAdapter
+        }
     }
 
     override fun onDestroyView() {
@@ -59,7 +49,36 @@ class DebtsViewImplementation :
     }
 
     override fun setData(data: List<DebtsData>) {
-        adapter.setData(data)
-        adapter.notifyDataSetChanged()
+        debtsAdapter.update(data)
+        debtsAdapter.notifyDataSetChanged()
+    }
+
+    override fun showLoading() {
+        binding.loading.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        binding.loading.visibility = View.GONE
+    }
+
+    override fun showError(code: Int, description: String) {
+        Toast.makeText(context, "Error: $code $description", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showDeleteDialog() {
+        Toast.makeText(context, "DeleteDialog", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showChatScreen(idDebtor: Int) {
+        component().routingModule.navigationHolder.setNavigator(
+            AppNavigator(
+                requireActivity(),
+                R.id.fragment_container
+            )
+        )
+        component().routingModule.router.navigateTo(
+            Screens.Chat("Александр", 12000, "url")
+        )
+//        component().routingModule.router.navigateTo(Screens.Chat(idDebtor))
     }
 }
