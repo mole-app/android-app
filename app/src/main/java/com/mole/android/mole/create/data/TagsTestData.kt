d1820bde0a94d082abdbd27139cacd81bd73a776
Aleksandr.milakin <aleksandr.milakin@myoffice.team>
2022-09-02 10:28:42 +0300

    Change mock data.

--- app/src/main/java/com/mole/android/mole/create/data/TagsTestData.kt
+++ app/src/main/java/com/mole/android/mole/create/data/TagsTestData.kt
@@ -4,46 +4,78 @@ import com.mole.android.mole.create.model.TagPreview
 
 val tagsTestData = listOf(
     TagPreview(
-        name = "фогич",
-        count = 10
-    ),
-    TagPreview(
-        name = "рестики",
-        count = 27
-    ),
-    TagPreview(
-        name = "мальдивы",
+        name = "ресторан",
         count = 3
-    ),
-    TagPreview(
-        name = "карелия",
-        count = 4
     ),
     TagPreview(
         name = "картинг",
-        count = 15
-    ),
-    TagPreview(
-        name = "маник",
-        count = 31
-    ),
-    TagPreview(
-        name = "поездка_прошлым_летом",
-        count = 2
-    ),
-    TagPreview(
-        name = "уже_не_помню",
         count = 1
     ),
     TagPreview(
-        name = "долг",
+        name = "поход",
         count = 7
     ),
     TagPreview(
-        name = "скинь_заебал",
+        name = "пикник",
+        count = 2
+    ),
+    TagPreview(
+        name = "Сочи",
+        count = 27
+    ),
+    TagPreview(
+        name = "такси",
+        count = 5
+    ),
+    TagPreview(
+        name = "поездка",
         count = 3
-    ),TagPreview(
-        name = "жфвтпо",
-        count = 14
-    )
+    ),
+    TagPreview(
+        name = "шаверма",
+        count = 10
+    ),
+//    TagPreview(
+//        name = "фогич",
+//        count = 10
+//    ),
+//    TagPreview(
+//        name = "рестики",
+//        count = 27
+//    ),
+//    TagPreview(
+//        name = "мальдивы",
+//        count = 3
+//    ),
+//    TagPreview(
+//        name = "карелия",
+//        count = 4
+//    ),
+//    TagPreview(
+//        name = "картинг",
+//        count = 15
+//    ),
+//    TagPreview(
+//        name = "маник",
+//        count = 31
+//    ),
+//    TagPreview(
+//        name = "поездка_прошлым_летом",
+//        count = 2
+//    ),
+//    TagPreview(
+//        name = "уже_не_помню",
+//        count = 1
+//    ),
+//    TagPreview(
+//        name = "долг",
+//        count = 7
+//    ),
+//    TagPreview(
+//        name = "скинь_заебал",
+//        count = 3
+//    ),TagPreview(
+//        name = "жфвтпо",
+//        count = 14
+//    )
 )
