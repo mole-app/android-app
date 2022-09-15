fd4c0545f06e0ed5ff8eb58c4cee289ff00cd339
Aleksandr.milakin <aleksandr.milakin@myoffice.team>
2022-09-15 22:32:32 +0300

    Fix text and image for about name.

--- app/src/main/java/com/mole/android/mole/about/view/NameAboutViewImpl.kt
+++ app/src/main/java/com/mole/android/mole/about/view/NameAboutViewImpl.kt
@@ -1,10 +1,18 @@
 package com.mole.android.mole.about.view
 
+import android.os.Bundle
+import android.view.View
 import com.mole.android.mole.MoleBaseFragment
 import com.mole.android.mole.databinding.ViewNameAboutBinding
+import com.mole.android.mole.dp
+import com.mole.android.mole.setupCornerRadius
 import com.mole.android.mole.ui.actionbar.MoleActionBar
 
 class NameAboutViewImpl  : MoleBaseFragment<ViewNameAboutBinding>(ViewNameAboutBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.imageNameAbout.setupCornerRadius(12f.dp)
+    }
 
     override fun getToolbar(): MoleActionBar {
         return binding.moleNameAboutToolbar
