816cf72c2ac02a87970906699498eced38b8046e
omy-q <olya_aparina@mail.ru>
2023-02-16 21:09:25 +0300

    create ui data binding

--- app/src/main/java/com/mole/android/mole/ui/UiState.kt
+++ app/src/main/java/com/mole/android/mole/ui/UiState.kt
@@ -2,10 +2,30 @@ package com.mole.android.mole.ui
 
 import com.mole.android.mole.web.service.MoleError
 
-data class UiState<out T>(
-    val isLoading: Boolean = false,
+open class UiState<out T>(
+    val isLoading: Boolean,
+    val isErrorVisible: Boolean,
+    val isContentVisible: Boolean,
     val error: MoleError? = null,
-    val isErrorVisible: Boolean = false,
-    val content: T? = null,
-    val isContentVisible: Boolean = false
+    val content: T? = null
+)
+
+class Loading<out T> : UiState<T>(
+    isLoading = true,
+    isErrorVisible = false,
+    isContentVisible = true
+)
+
+class Error<out T>(error: MoleError) : UiState<T>(
+    isLoading = false,
+    isErrorVisible = true,
+    isContentVisible = false,
+    error = error
+)
+
+class Content<out T>(data: T) : UiState<T>(
+    isLoading = false,
+    isErrorVisible = false,
+    isContentVisible = true,
+    content = data
 )
