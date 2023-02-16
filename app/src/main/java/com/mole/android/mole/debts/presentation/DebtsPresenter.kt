353abeb3bea97516422730b57f7187093d23788c
omy-q <olya_aparina@mail.ru>
2023-02-16 21:10:41 +0300

    add data binding to DebtsScreen

--- app/src/main/java/com/mole/android/mole/debts/presentation/DebtsPresenter.kt
+++ app/src/main/java/com/mole/android/mole/debts/presentation/DebtsPresenter.kt
@@ -5,6 +5,9 @@ import com.mole.android.mole.debts.data.DebtorData
 import com.mole.android.mole.debts.data.DebtsData
 import com.mole.android.mole.debts.model.DebtsModel
 import com.mole.android.mole.debts.view.DebtsView
+import com.mole.android.mole.ui.Content
+import com.mole.android.mole.ui.Error
+import com.mole.android.mole.ui.Loading
 import com.mole.android.mole.ui.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,7 +18,8 @@ class DebtsPresenter(
     private val model: DebtsModel
 ) : MoleBasePresenter<DebtsView>() {
 
-    private val mutableState = MutableStateFlow(UiState<DebtsData>())
+    private val mutableState: MutableStateFlow<UiState<DebtsData>> =
+        MutableStateFlow(Loading())
     val state: StateFlow<UiState<DebtsData>> = mutableState.asStateFlow()
 
     override fun attachView(view: DebtsView) {
@@ -38,36 +42,14 @@ class DebtsPresenter(
 
     private fun dataLoading() {
         withScope {
-            mutableState.value = UiState(
-                isLoading = true,
-                isContentVisible = true,
-                isErrorVisible = false
-            )
+            mutableState.value = Loading()
             launch {
                 model.loadDebtsData()
                     .withResult { result ->
-                        mutableState.value = UiState(
-                            isLoading = false,
-                            content = result,
-                            isContentVisible = true,
-                            isErrorVisible = false
-                        )
-//                        withView { view ->
-//                            view.setData(result)
-//                            view.hideLoading()
-//                        }
+                        mutableState.value = Content(result)
                     }
                     .withError { error ->
-                        mutableState.value = UiState(
-                            isLoading = false,
-                            error = error,
-                            isContentVisible = false,
-                            isErrorVisible = true
-                        )
-//                        withView { view ->
-//                            view.hideLoading()
-//                            view.showError(error.code, error.description)
-//                        }
+                        mutableState.value = Error(error)
                     }
             }
         }
--- app/src/main/java/com/mole/android/mole/debts/view/DebtsView.kt
+++ app/src/main/java/com/mole/android/mole/debts/view/DebtsView.kt
@@ -5,9 +5,6 @@ import com.mole.android.mole.debts.data.DebtorData
 import com.mole.android.mole.debts.data.DebtsData
 
 interface DebtsView : MoleBaseView {
-//    fun setData(data: DebtsData)
-//    fun showLoading(isVisible: Boolean)
-//    fun showError(code: Int, description: String)
     fun showRepayScreen(data: DebtorData)
     fun showChatScreen(idDebtor: Int)
 }
